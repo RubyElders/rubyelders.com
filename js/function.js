e570e68dd17c23424a741fc658da4751370e2e0b
simi <info@rubyelders.com>
2025-03-16 15:54:06 +0000

    Deploying to gh-pages from @ RubyElders/rubyelders.com@b54680a728a61b24c82bac1db8479c53108922e0 🚀

--- js/function.js
+++ js/function.js
@@ -4,3 +4,15 @@
 $(function(){
 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    const hat = document.getElementById("hat");
+    hat.style.animation = "wave-hat 1.5s ease-in-out forwards";
+
+    // clear the animation for hover
+    setTimeout(() => {
+      hat.style.animation = "";
+    }, 1500);
+  }, 1000);
+});
